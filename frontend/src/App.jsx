987219fc884457626873f41f9eeb4c9ac1892463
cazapan <totogomez1000@gmail.com>
2026-05-29 02:14:10 -0300

    Se añadieron mas llamadas a la API

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,10 @@ import { useState } from 'react'
 import axios from "axios"
 import GetBanks from './components/getAllBanks'
 import ChangeBankName from './components/changeBankName'
+import RegisterPersonCentralBank from './components/registerPersonCentralBank'
+import SearchPersonByCBU from './components/searchPersonByCBU'
+import AsingAliasToPerson from './components/asingAliasToPerson'
+import SearchPersonByAlias from './components/searchPersonByAlias'
 import './App.css'
 
 
@@ -11,6 +15,10 @@ export default function App() {
     <>
       <GetBanks></GetBanks>
       <ChangeBankName></ChangeBankName>
+      <RegisterPersonCentralBank></RegisterPersonCentralBank>
+      <SearchPersonByCBU></SearchPersonByCBU>
+      <AsingAliasToPerson></AsingAliasToPerson>
+      <SearchPersonByAlias></SearchPersonByAlias>
     </>
   )
 
--- frontend/src/components/registerPersonCentralBank.jsx
+++ frontend/src/components/registerPersonCentralBank.jsx
@@ -10,13 +10,30 @@ function RegisterPersonCentralBank() {
     })
 
     const handleChange = (e) => {
-        
+        setPersona({
+            ...persona,
+            [e.target.name]: e.target.value
+        })
     }
+
+    const postPersona = async () => {
+        try {
+            const { data } = await api.post('/persons', persona);
+            console.log('Persona registrada:', data);
+        } catch (error) {
+            console.error('Error al registrar la persona:', error);
+        }
+    };
 
     return(
         <div>
-
+            <input placeholder="Nombre" name="nombre" value={persona.nombre} onChange={handleChange} />
+            <input placeholder="Apellido" name="apellido" value={persona.apellido} onChange={handleChange} />
+            <input placeholder="DNI" name="dni" value={persona.dni} onChange={handleChange} />
+            <button onClick={postPersona}>Registrar Persona</button>
         </div>
     )
 
 }
+
+export default RegisterPersonCentralBank
